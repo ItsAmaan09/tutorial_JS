51b85e11db21bbf9b8ec70234ebfb2c391a8e15b
Mohammed Amaan <93517033+ItsAmaan09@users.noreply.github.com>
2024-07-03 12:16:15 +0000

    dataTypes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tutorial_JS
 Here we learn the javascript topics from basic to advanced!!!
+
+documentation for ECMA standard:-
+https://tc39.es/ecma262/#sec-intro
---
+++ tutorials/03_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // all js as new version
+
+// alert(3+3);  we are using nodeJs not browser , so error comes
+
+
+// number, bigint, string, boolean , undefined, null, symbol 
+
+let age =  90071992547409921;
+let isValid = undefined;
+let myPhone = null;
+console.log(typeof age);
+console.log(typeof isValid);
+console.log(typeof myPhone);
